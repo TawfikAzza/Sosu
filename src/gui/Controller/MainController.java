d24ea95ca1a19fea76856a8c819ef4023c3fc6c0
imad0123456789 <imad0112@easv365.dk>
2022-04-29 11:52:58 +0200

    01

--- src/gui/Controller/MainController.java
+++ src/gui/Controller/MainController.java
@@ -1,4 +1,24 @@
 package gui.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField userField;
+
+
 }
