c83d456cf4908c9b421f68ad2d89fa7968e05d64
Renaldinho <rena0968@easv365.dk>
2022-04-30 10:59:51 +0200

    New Views

---
+++ src/gui/Controller/ManageCitizensController.java
@@ -0,0 +1,13 @@
+package gui.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageCitizensController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
