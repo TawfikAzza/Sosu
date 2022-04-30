58664685968af6d6075bd1457893a63d84d01a8d
imad0123456789 <imad0112@easv365.dk>
2022-04-30 15:35:50 +0200

    4

--- src/bll/UserManger.java
+++ src/bll/UserManger.java
@@ -3,7 +3,18 @@ package bll;
 import be.User;
 import dal.db.UsersDAO;
 
+import java.io.IOException;
+
 public class UserManger implements UserInterface {
+
+    UsersDAO usersDAO;
+    {
+        try{
+            usersDAO = new UsersDAO();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
