adc653a9f196a4a88662a142e57a7f37e9b45dfd
imad0123456789 <imad0112@easv365.dk>
2022-05-03 00:40:55 +0200

    MedicineList

---
+++ src/be/MedicineList.java
@@ -0,0 +1,14 @@
+package be;
+
+import java.util.List;
+
+public class MedicineList {
+    private int id;
+    private int citizenID;
+    private String medicine;
+    private List<MedicineList> medicineLists;
+
+
+
+
+}
