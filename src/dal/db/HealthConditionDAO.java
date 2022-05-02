937156e74e6231ea5a984ebcc08b288becb44a37
TawfikAzza <tawf0021@easv365.dk>
2022-05-02 16:18:21 +0200

    Adding comments on the report

--- src/dal/db/HealthConditionDAO.java
+++ src/dal/db/HealthConditionDAO.java
@@ -23,6 +23,15 @@ public class HealthConditionDAO {
     public HealthConditionDAO() throws IOException {
         cm = new ConnectionManager();
     }
+    /**
+     * Author : Tawfik
+     *
+     * Method in charge of returning a list of Main healthCategory object
+     * with the subcategories associated to it (the subcategories are stored in
+     * the subCategories List with is held in the HealthCondition object.)
+     * This method is called by the HealthCondition management Controller in charge of the input of values
+     * for the citizen.
+     * *****/
 
     public List<HealthCategory> getAllCategoriesTree() throws SQLException {
         List<HealthCategory> healthCategories = new ArrayList<>();
@@ -42,6 +51,18 @@ public class HealthConditionDAO {
         healthCategories = mainHealthCategories.values().stream().toList();
         return healthCategories;
     }
+
+    /***
+     * Author: Tawfik
+     *
+     * Method in charge of retrieving the HealthCondition subcategories and returning it as
+     * an ArrayList of HealthCategory object.
+     * This method is called by either the Report on health controller via the getConditionsFromCitizen
+     * method, or by
+     * the Controller in charge of managing the input of values in the conditions
+     * of the citizen.
+     *
+     * **/
     public List<HealthCategory> getAllSubCategories() throws SQLException {
         List<HealthCategory> healthCategories = new ArrayList<>();
         HashMap<Integer,HealthCategory> mainHealthCategories = getAllMainHealthCategories();
@@ -54,6 +75,7 @@ public class HealthConditionDAO {
             while (rs.next()) {
                 HealthCategory healthCategory = new HealthCategory(rs.getInt("id"),rs.getString("name"));
                 healthCategory.setSid(rs.getInt("sid"));
+                healthCategory.setExample(rs.getString("example"));
                 healthCategories.add(healthCategory);
             }
 
@@ -61,7 +83,16 @@ public class HealthConditionDAO {
 
         return healthCategories;
     }
-//TODO: FINISH THE Object HealthCategory Creation and add the rest of the attributes in it !!!
+    /***
+     * Author: Tawfik
+     *
+     * Method in charge of retrieving the Main Health category and returning it as
+     * an HashMap<Integer,HealthCategory>.
+     * This method is called by either the Report on health controller or
+     * the Controller in charge of managing the input of values in the conditions
+     * of the citizen.
+     *
+     * **/
     public HashMap<Integer,HealthCategory> getAllMainHealthCategories() throws SQLException {
         HashMap<Integer,HealthCategory> allCategories = new HashMap<>();
         try (Connection connection = cm.getConnection()) {
@@ -76,7 +107,12 @@ public class HealthConditionDAO {
         }
         return allCategories;
     }
-
+    /**
+     * Author: Tawfik
+     *
+     * Method that retrieve a specific condition associated to a subcategory form the
+     * Conditions table.
+     * ****/
     public Condition getCondition(HealthCategory healthCategory, Citizen citizen) throws SQLException {
         Condition conditionSearched = null;
         try (Connection connection = cm.getConnection()) {
@@ -97,7 +133,10 @@ public class HealthConditionDAO {
         }
         return conditionSearched;
     }
-
+    /***
+     * Author : Tawfik
+     * Add a condition in the database.
+     * **/
     public void addCondition(Condition condition) throws SQLException {
         try (Connection connection = cm.getConnection()) {
             String sqlInsert = "INSERT INTO CONDITIONS VALUES(?, ?, ?, ?, ?, ?)";
@@ -113,6 +152,14 @@ public class HealthConditionDAO {
 
         }
     }
+    /***
+     * Author: Tawfik
+     * Update the condition of a citizen
+     * This method is pretty straightforward
+     * Note however that in order to find the right condition, I do not use the ID of the condition
+     * but rather the ID of the Citizen as well as the ID of the subcategory it is associated to.
+     * This is made possible because only one condition exists for one subcategory.
+     */
 
     public void updateCondition(Condition condition) throws SQLException {
         try (Connection connection = cm.getConnection()) {
@@ -133,7 +180,7 @@ public class HealthConditionDAO {
     }
 
     /***
-     * Creator : Tawfik
+     * Author : Tawfik
      * This method is in charge of retrieving all the sensible information pertaining to the
      * Health Condition of a Citizen and rearranging them as cleanly as possible for later use
      * in the displaying of the Health condition side of the report to the student.
