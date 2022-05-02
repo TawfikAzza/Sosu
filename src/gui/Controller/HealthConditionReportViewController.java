8db51ac774a51a5f20c03a882445a105b7869fd8
TawfikAzza <tawf0021@easv365.dk>
2022-05-02 17:14:33 +0200

    Adding comments on the report

--- src/gui/Controller/HealthConditionReportViewController.java
+++ src/gui/Controller/HealthConditionReportViewController.java
@@ -19,7 +19,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-
+/**
+ * Author : Tawfik
+ * This controller is in charge of displaying a report retracing all the values the nurse in training/teacher
+ * inputted for this specific citizen
+ * The nature of the project as well as the data to be managed forces it to be a dynamically generated page.
+ *
+ * ***/
 public class HealthConditionReportViewController implements Initializable {
     @FXML
     private ScrollPane scrollPane;
@@ -36,11 +42,24 @@ public class HealthConditionReportViewController implements Initializable {
             DisplayMessage.displayError(e);
         }
     }
-
+    /**
+     * Method which sets the citizen to be displayed.
+     * */
     public void setCurrentCitizen(Citizen currentCitizen) {
         this.currentCitizen = currentCitizen;
         displayCitizenReport();
     }
+    /**
+     * This method is quite straightforward, it search through the
+     * HashMap<Integer(Id of main category),List<Pair<HealthCategory, HealthCategory>>>
+     * and displays it for the user to see.
+     * The creation of this HashMap is available in the HealthConditionDAO class
+     * It alse make use of another HashMap<Integer,HealthCategory> to make the connection between the id of the main category
+     * and the name of the main category for ease of reading.
+     *
+     * Note: A nice addition to it could be to create a .doc of pdf report based on these values, but as has not been
+     * demanded, we will wait and ask the customer about it during the first sprint review.
+     * ***/
     public void displayCitizenReport() {
         HashMap<Integer, List<Pair<HealthCategory, Condition>>> hashMap = null;
         HashMap<Integer,HealthCategory> categoryHashMap = new HashMap<>();
@@ -51,16 +70,22 @@ public class HealthConditionReportViewController implements Initializable {
             DisplayMessage.displayError(e);
         }
 
-
+        //Parsing through the HashMap
         for (Map.Entry<Integer, List<Pair<HealthCategory, Condition>>> entry : hashMap.entrySet()) {
             Integer sid = (Integer) entry.getKey();
             List<Pair<HealthCategory,Condition>> list = (List<Pair<HealthCategory,Condition>>)entry.getValue();
+            //Creating a Vbox for display purpose as well as a lable associated which hold the name of the Main category
             VBox vBox = new VBox();
             Label mainCat = new Label();
             mainCat.setStyle("-fx-font-weight: bold");
+            //We add the Main Category label to the Vbox
             vBox.getChildren().add(mainCat);
             mainCat.setText(categoryHashMap.get(sid).getName());
+            //we then parse the subcategories held in the Pair List.
             for (Pair<HealthCategory,Condition> pair : list){
+                //we create a TextFlow holding the currently parses subcategory and its value
+                //and add it in the Vbox
+                //Note that this is only temporary but as don't know what the customer wants displayed,
                 TextFlow textFlow;
                 Text text = new Text();
                 text.setText(pair.getKey().getName()+": \n "
@@ -70,7 +95,6 @@ public class HealthConditionReportViewController implements Initializable {
                         +"\n Status : "+pair.getValue().getStatus());
 
 
-               // vBox.getChildren().add(subCat);
                 textFlow = new TextFlow(text);
                 vBox.getChildren().add(textFlow);
                 
