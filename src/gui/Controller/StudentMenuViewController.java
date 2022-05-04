05bb69f385fc65052e445fe0748e4f499276b094
TawfikAzza <tawf0021@easv365.dk>
2022-05-04 13:56:59 +0200

    Student Menu draft and implementation

--- src/gui/Controller/StudentMenuViewController.java
+++ src/gui/Controller/StudentMenuViewController.java
@@ -154,6 +154,6 @@ public class StudentMenuViewController implements Initializable {
     }
 
 
-    public void openObservation(ActionEvent actionEvent) {
+    public void openObservation() {
     }
 }
