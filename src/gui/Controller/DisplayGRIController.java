08ca5f3c74680a2d51aa31b35624d1441f794032
imad0123456789 <imad0112@easv365.dk>
2022-05-12 12:25:30 +0200

    Report

--- src/gui/Controller/DisplayGRIController.java
+++ src/gui/Controller/DisplayGRIController.java
@@ -1,5 +1,12 @@
 package gui.Controller;
 
+import be.Citizen;
+import be.GeneralInfo;
+import be.InfoCategory;
+import bll.GIReportManger;
+import bll.exceptions.GeneralInfoException;
+import bll.util.GlobalVariables;
+import gui.Model.GIReportModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,13 +30,45 @@ public class DisplayGRIController implements Initializable {
     @FXML
     private Button btnClose;
 
+    private GIReportManger giReportManger;
+    private GIReportModel giReportModel;
+    private InfoCategory selectedInfoCategory;
+    private Citizen citizen;
+
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            giReportManger = new GIReportManger();
+
+        } catch (GeneralInfoException e) {
+            e.printStackTrace();
+        }
+        displayReport(citizen,selectedInfoCategory);
 
     }
+
+
+    public void displayReport(Citizen citizen, InfoCategory selectedInfoCategory){
+
+        this.citizen = citizen;
+        this.selectedInfoCategory = selectedInfoCategory;
+
+            lblFName.setText(GlobalVariables.getSelectedCitizen().getFName());
+            lblLName.setText(GlobalVariables.getSelectedCitizen().getLName());
+            lblAdress.setText(GlobalVariables.getSelectedCitizen().getAddress());
+            //lblBirthdate.setText(GlobalVariables.getSelectedCitizen().getBirthDate());
+            //lblPhone.setText(GlobalVariables.getSelectedCitizen().getPhoneNumber());
+            //lblSchool.setText(GlobalVariables.getSelectedCitizen().getSchoolID());
+
+
+
+        }
+
+
+
 
 
     public void CloseAction() {
--- src/gui/Model/GIReportModel.java
+++ src/gui/Model/GIReportModel.java
@@ -1,16 +1,26 @@
 package gui.Model;
 
+import be.Citizen;
+import be.InfoCategory;
 import bll.GIReportManger;
-import bll.GInfoManager;
+import bll.exceptions.CitizenException;
 import bll.exceptions.GeneralInfoException;
+
 
 public class GIReportModel {
 
+
     private GIReportManger giReportManger;
 
-    public GIReportModel() throws GeneralInfoException {
+    public GIReportModel() throws GeneralInfoException, CitizenException {
         giReportManger = new GIReportManger();
     }
 
 
+    public void getGiReportManger(Citizen citizen, InfoCategory selectedInfoCategory) {
+        giReportManger.getGIReport(citizen, selectedInfoCategory);
+    }
 }
+
+
+
