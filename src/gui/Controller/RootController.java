363e646e269c719799ddafca30a6bb3ad3901054
silamin <amine.aouina.lp.4@gmail.com>
2022-05-18 22:11:27 +0200

    in progress

--- src/gui/Controller/RootController.java
+++ src/gui/Controller/RootController.java
@@ -11,21 +11,27 @@ import javafx.scene.control.TabPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
 
 
+    public GridPane mainGPane;
+    public JFXHamburger iconHamburgerMGP;
+    public GridPane drawerGPane;
+    public JFXHamburger iconHamburgerDGP;
+    public StackPane stackPane;
     @FXML
     private VBox iconsBox;
     @FXML
     private AnchorPane mainPane;
-    @FXML
-    private JFXHamburger iconHamburger;
+
     @FXML
     private JFXDrawer drawer;
 
@@ -39,11 +45,19 @@ public class RootController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initUser();
+        List<JFXHamburger>allHamBurgers= List.of(iconHamburgerDGP,iconHamburgerMGP);
+        for (JFXHamburger iconHamburger : allHamBurgers)
         iconHamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
             if(drawer.isOpened()) {
+                GridPane gridPane = new GridPane();
+                gridPane.getChildren().add(mainGPane);
+                stackPane.getChildren().add(gridPane);
                 drawer.close();
             }
             else {
+                GridPane gridPane = new GridPane();
+                gridPane.getChildren().add(drawerGPane);
+                stackPane.getChildren().add(gridPane);
                 drawer.open();
             }
         });
